3669e02285be3485bba52ccebc31ddaa3ec37c8a
William Grzybowski <wg@FreeBSD.org>
2018-11-08 13:22:22 -0200

    Default to python3
    2 is no longer supported.
    
    (cherry picked from commit 56c93b99b094badd4350a0b0de4c7410fb736021)

--- diff_packages/diff_packages.py
+++ diff_packages/diff_packages.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env /usr/local/bin/python
-from __future__ import print_function
+#!/usr/bin/env python3
 
 import os, sys
 import getopt
--- freenas-update/freenas-update.py
+++ freenas-update/freenas-update.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env /usr/local/bin/python
-from __future__ import print_function
-
+#!/usr/bin/env python3
 import getopt
 import logging
 import os
