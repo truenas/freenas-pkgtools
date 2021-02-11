b7f551d6bbbce008d1beb737c6cb529930f062ca
E314c <E314c@users.noreply.github.com>
2021-02-11 18:14:32 +0100

    Update Configuration.py
    I was trying to installing TrueNAS core and was getting the "%{0} has invalid checksum" on the `base-os-(...).tgz`. It would be useful to be able to see what checksum it has calculated and what it's verifying against.
    
    This could be moved into a debug log, but I've not found a way of enabling that during installation from a boot disk.

--- lib/Configuration.py
+++ lib/Configuration.py
@@ -1223,7 +1223,7 @@ class Configuration(object):
                             if h == search_attempt["Checksum"]:
                                 return file
                             else:
-                                pkg_exception = Exceptions.ChecksumFailException("%{0} has invalid checksum".format(search_attempt["Filename"]))
+                                pkg_exception = Exceptions.ChecksumFailException("%{0} has invalid checksum: Expected {1} Got {2}".format(search_attempt["Filename"], search_attempt["Checksum"], h))
                         else:
                             # No checksum for the file, so we'll just go with it.
                             return file
@@ -1262,7 +1262,7 @@ class Configuration(object):
                         log.debug("Checksum doesn't match, removing file")
                         if save_name:
                             os.unlink(save_name)
-                        pkg_exception = Exceptions.ChecksumFailException("%{0} has invalid checksum".format(pFile))
+                        pkg_exception = Exceptions.ChecksumFailException("%{0} has invalid checksum: Expected {1} Got {2}".format(pFile, search_attempt["Checksum"], h))
                 else:
                     # No checksum for the file, so we just go with it
                     return file
